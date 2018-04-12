264449687fd40d7a9aa1ba71d5c6c0e536bc9cc7
MarcosWilliam0712 <marcoswilliam0712@gmail.com>
2018-04-11 23:38:17 -0300

    Função Tamanho e TestTamanho consertados

--- src/test/scala/br/unb/cic/epl/TestTamanho.scala
+++ src/test/scala/br/unb/cic/epl/TestTamanho.scala
@@ -24,35 +24,36 @@ class TestTamanho extends FlatSpec with Matchers with GivenWhenThen with BeforeA
 
   before
   {
+    lit100 = new Core.Literal(100) with Eval.Literal
+    lit500 = new Core.Literal(500) with Eval.Literal
 
     sum = new AddEval.Add(lit100, lit500)
     sub = new SubEval.Sub(lit100, lit500)
     mult = new MultiEval.Multi(lit100, lit500)
 
-
-
   }
 
-  it should "return String 2 when we call print(tam_sum.exp_sum)" in
+  it should "return Integer 2 when we call tam_sum.exp_sum()" in
     {
       var tam_sum = new Tamanho.Tamanho(sum.print())
       tam_sum.tam_sum()
-      print(tam_sum.exp_sum)
 
-      print(tam_sum.exp_sum) should be ("2")
+      tam_sum.exp_sum should be (2)
     }
 
-  it should "return String 2 when we call tam_sub.tam_sub(sub.print())" in
+  it should "return String 2 when we call tam_sub.exp_sub()" in
     {
       var tam_sub = new Tamanho.Tamanho(sub.print())
+      tam_sub.tam_sub()
 
-      tam_sub.tam_sub(sub.print()) should be ("2")
+      tam_sub.exp_sub should be (2)
     }
 
-  it should "return String 2 when we call tam_mult.tam_mult(mult.print())" in
+  it should "return String 2 when we call tam_mult.exp_mult()" in
     {
       var tam_mult = new Tamanho.Tamanho(mult.print())
+      tam_mult.tam_mult()
 
-      tam_mult.tam_mult(mult.print()) should be ("2")
+      tam_mult.exp_mult should be (2)
     }
 }
